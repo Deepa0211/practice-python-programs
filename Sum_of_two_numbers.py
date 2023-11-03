502dbe547dd6b68f4f5204f7f769723706329422
Deepa0211 <deepa.menon.786@gmail.com>
2023-11-03 07:29:24 +0530

    Added Code

---
+++ Sum_of_two_numbers.py
@@ -0,0 +1,6 @@
+# Write a program to find the sum of two numbers.
+
+num_1 = int(input("Enter first number: "))
+num_2 = int(input("Enter second number: "))
+
+print("Sum of two numbers is:", num_1 + num_2)
