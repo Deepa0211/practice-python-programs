2eee7b228e6174689f8a934e5c59d043a1201151
Deepa0211 <deepa.menon.786@gmail.com>
2023-11-04 07:25:05 +0530

    Added Code

---
+++ Max_three_numbers.py
@@ -0,0 +1,6 @@
+number1 = int(input("Enter first number: "))
+number2 = int(input("Enter second number: "))
+number3 = int(input("Enter third number: "))
+
+# using max function
+print("Maximum of three numbers is", max(number1, number2, number3))
