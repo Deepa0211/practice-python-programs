138ecdcdd40700e6c61b67b00ada4a9c0bf34f74
Deepa0211 <deepa.menon.786@gmail.com>
2023-11-04 07:13:09 +0530

    Added Code

---
+++ Max_two_numbers.py
@@ -0,0 +1,12 @@
+number1 = int(input("Enter first number: "))
+number2 = int(input("Enter second number: "))
+
+# using max function
+print("Maximum of two number is", max(number1, number2))
+
+# Traditional approach 
+# if number1 > number2:
+#     print("First number is greater than Second number")
+# else:
+#     print("Second number is greater than First number")
+    
