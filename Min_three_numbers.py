5ba7f6f4ab093e609d88a1aa1349d6e9535b6ec8
Deepa0211 <deepa.menon.786@gmail.com>
2023-11-05 07:12:07 +0530

    Added Code

---
+++ Min_three_numbers.py
@@ -0,0 +1,7 @@
+# Write a program to find a minimum of three numbers.
+number1 = int(input("Enter first number: "))
+number2 = int(input("Enter second number: "))
+number3 = int(input("Enter third number: "))
+
+# using min function
+print("Minimum of three numbers", min(number1, number2, number3))
